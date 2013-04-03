65bcc8202895a3d9368d3de37a98851defeea777
Tural Gurbanov <madcat1991@gmail.com>
2013-04-03 22:50:13 +0400

    Новый способ задания условий

--- clustering_exec.py
+++ clustering_exec.py
@@ -5,25 +5,35 @@ import math
 from cluster import get_clusters
 
 
-def prepare_data(file_path, min_log, delimiter="|"):
+def prepare_data(file_path, min_count, min_adj_count, delimiter="|"):
     """Читает файл, содержащий набор прилаг + существительное и собирает словарь следующего типа:
     {сущ1: {прилаг1: ln(кол-во встреч), прилаг2: ln(кол-во встре),...}, ...}
-    Для существительного берутся только прилагательные чей логорифм от количества встреч > min_log
+
+    Берутся только комбинации, которые встречаются не менее min_count раз.
+    Из всех существительных, оставляются только те, у которых количество связных прилагательных
+    не менее min_adj_count
     """
     res = {}
     f = open(file_path, "r")
     for line in f:
-        if line:
-            parts = line.strip().split(delimiter)
-            #TODO написать адекватнее
-            if len(parts) == 2:
-                phrase_parts = parts[0].split()
+        parts = line.strip().split(delimiter)
+        if len(parts) >= 2:
+            try:
+                count = int(parts[-1])
+            except TypeError:
+                continue
+
+            if count > min_count:
+                #на случай, если внутри был разделитель
+                phrase = delimiter.join(parts[:-1])
+                phrase_parts = phrase.split()
                 if len(phrase_parts) == 2:
                     adj, noun = phrase_parts
-                    count = float(parts[1])
-                    if count > 0 and math.log(count) > min_log:
-                        res.setdefault(noun, {})
-                        res[noun][adj] = math.log(count)
+                    res.setdefault(noun, {})
+                    res[noun][adj] = math.log(count)
+    for noun in res.keys():
+        if len(res[noun]) < min_adj_count:
+            res.pop(noun)
     return res
 
 
@@ -71,13 +81,16 @@ if __name__ == "__main__":
                         help="Путь до файла с csv")
     parser.add_argument("-d", "--dot-file", dest="dot_file_path", type=str, required=True,
                         help="Файл в который будет сохранено dot-представление кластера")
-    parser.add_argument("-t", "--min-log", dest="min_log", type=float, default=0,
-                        help="Берутся только строки, логорифм от количества встреч которого больше введенного значения."
-                             "По умолчанию 0")
+    parser.add_argument("-t", "--min-count", dest="min_count", type=int, default=1,
+                        help="Берутся только комбинации, количества встреч которых не меньше введенного значения."
+                             "По умолчанию 1")
+    parser.add_argument("-a", "--min-adj-count", dest="min_adj_count", type=int, default=5,
+                        help="Рассматриваюся только существительные, количество связных прилагательных у которых, "
+                             "не меньше введеного значения. По умолчанию 5")
 
     args = parser.parse_args()
 
-    data_dict = prepare_data(args.csv_file_path, args.min_log)
+    data_dict = prepare_data(args.csv_file_path, args.min_count, args.min_adj_count)
     keys_list, matrix = get_nouns_similarity_matrix(data_dict)
     cluster = get_clusters(matrix)
 
